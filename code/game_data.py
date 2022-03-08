14450c4b3a9d298e72dfee62c993945de9248bbc
griffin1995 <51092224+griffin1995@users.noreply.github.com>
2022-03-08 18:54:43 +0000

    full game

---
+++ code/game_data.py
@@ -0,0 +1,95 @@
+level_0 = {
+    'terrain': '..//game/levels/0/level_0_terrain.csv',
+    'coins': '..//game/levels/0/level_0_coins.csv',
+    'foreground palms': '..//game/levels/0/level_0_fg_palms.csv',
+    'background palms': '..//game/levels/0/level_0_bg_palms.csv',
+    'crates': '..//game/levels/0/level_0_crates.csv',
+    'enemies': '..//game/levels/0/level_0_enemies.csv',
+    'constraints': '..//game/levels/0/level_0_constraints.csv',
+    'player': '..//game/levels/0/level_0_player.csv',
+    'grass': '..//game/levels/0/level_0_grass.csv',
+    'node_pos': (110, 400),
+    'unlock': 1,
+    'node_graphics': '..//game/graphics/overworld/0'
+
+}
+level_1 = {
+    'terrain': '..//game/levels/1/level_1_terrain.csv',
+    'coins': '..//game/levels/1/level_1_coins.csv',
+    'foreground palms': '..//game/levels/1/level_1_fg_palms.csv',
+    'background palms': '..//game/levels/1/level_1_bg_palms.csv',
+    'crates': '..//game/levels/1/level_1_crates.csv',
+    'enemies': '..//game/levels/1/level_1_enemies.csv',
+    'constraints': '..//game/levels/1/level_1_constraints.csv',
+    'player': '..//game/levels/1/level_1_player.csv',
+    'grass': '..//game/levels/1/level_1_grass.csv',
+    'node_pos': (300, 220),
+    'unlock': 2,
+    'node_graphics': '..//game/graphics/overworld/1'
+}
+level_2 = {
+    'terrain': '..//game/levels/2/level_2_terrain.csv',
+    'coins': '..//game/levels/2/level_2_coins.csv',
+    'foreground palms': '..//game/levels/2/level_2_fg_palms.csv',
+    'background palms': '..//game/levels/2/level_2_bg_palms.csv',
+    'crates': '..//game/levels/2/level_2_crates.csv',
+    'enemies': '..//game/levels/2/level_2_enemies.csv',
+    'constraints': '..//game/levels/2/level_2_constraints.csv',
+    'player': '..//game/levels/2/level_2_player.csv',
+    'grass': '..//game/levels/2/level_2_grass.csv',
+    'node_pos': (480, 610),
+    'unlock': 3,
+    'node_graphics': '..//game/graphics/overworld/2'
+}
+level_3 = {
+    'terrain': '..//game/levels/3/level_3_terrain.csv',
+    'coins': '..//game/levels/3/level_3_coins.csv',
+    'foreground palms': '..//game/levels/3/level_3_fg_palms.csv',
+    'background palms': '..//game/levels/3/level_3_bg_palms.csv',
+    'crates': '..//game/levels/3/level_3_crates.csv',
+    'enemies': '..//game/levels/3/level_3_enemies.csv',
+    'constraints': '..//game/levels/3/level_3_constraints.csv',
+    'player': '..//game/levels/3/level_3_player.csv',
+    'grass': '..//game/levels/3/level_3_grass.csv',
+    'node_pos': (610, 350),
+    'unlock': 4,
+    'node_graphics': '..//game/graphics/overworld/3'
+}
+level_4 = {
+    'terrain': '..//game/levels/4/level_4_terrain.csv',
+    'coins': '..//game/levels/4/level_4_coins.csv',
+    'foreground palms': '..//game/levels/4/level_4_fg_palms.csv',
+    'background palms': '..//game/levels/4/level_4_bg_palms.csv',
+    'crates': '..//game/levels/4/level_4_crates.csv',
+    'enemies': '..//game/levels/4/level_4_enemies.csv',
+    'constraints': '..//game/levels/4/level_4_constraints.csv',
+    'player': '..//game/levels/4/level_4_player.csv',
+    'grass': '..//game/levels/4/level_4_grass.csv',
+    'node_pos': (880, 210),
+    'unlock': 5,
+    'node_graphics': '..//game/graphics/overworld/4'
+}
+level_5 = {
+    'terrain': '..//game/levels/5/level_5_terrain.csv',
+    'coins': '..//game/levels/5/level_5_coins.csv',
+    'foreground palms': '..//game/levels/5/level_5_fg_palms.csv',
+    'background palms': '..//game/levels/5/level_5_bg_palms.csv',
+    'crates': '..//game/levels/5/level_5_crates.csv',
+    'enemies': '..//game/levels/5/level_5_enemies.csv',
+    'constraints': '..//game/levels/5/level_5_constraints.csv',
+    'player': '..//game/levels/5/level_5_player.csv',
+    'grass': '..//game/levels/5/level_5_grass.csv',
+    'node_pos': (1050, 400),
+    'unlock': 5,
+    'node_graphics': '..//game/graphics/overworld/5'
+}
+
+
+levels = {
+    0: level_0,
+    1: level_1,
+    2: level_2,
+    3: level_3,
+    4: level_4,
+    5: level_5
+}
